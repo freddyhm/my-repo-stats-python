bffca320b504a00819996c567a391924c73e9571
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-15 11:39:16 -0400

    feat: format commit time

--- github.py
+++ github.py
@@ -1,6 +1,7 @@
 import requests
 import enum
-
+import datetime
+import pytz
 
 class Part_Of_Day(enum.Enum):
     MORNING = 1
@@ -27,7 +28,6 @@ def get_part_of_day(hour):
     else:
         return Part_Of_Day.INVALID_HOUR
 
-
 def get_github_commits():
     api_url = "https://api.github.com/repos/freddyhm/MyRepoStats/commits"
     response = requests.get(api_url)
@@ -36,9 +36,22 @@ def get_github_commits():
         data = response.json()
         print("API Response: ", data)
 
+        # get the hour of each commit and store it in a list
+        hours = []
+        for commit in data:
+            utc_date_string = commit["commit"]["author"]["date"]
+
+            utc_datetime = datetime.datetime.strptime(utc_date_string, "%Y-%m-%dT%H:%M:%SZ")
+
+            timezone = pytz.timezone("US/Eastern")
+            hour = utc_datetime.astimezone(timezone).hour
+
+            
+
         # format data in dictionary
         my_dict = dict(test=data) 
 
         return my_dict
     else:
         print("Error: ", response.status_code)
+
