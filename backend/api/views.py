77ef9a934d243d1497664bce907546292f7b68f7
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-26 17:02:25 -0400

    test(api): integrate quickly basic rest framework

---
+++ backend/api/views.py
@@ -0,0 +1,20 @@
+from api.services.commit_fetcher import get_part_of_day_percentage_of_commits
+from api.services.commit_fetcher import Part_Of_Day
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+
+    part_of_day_percentage_of_commits = get_part_of_day_percentage_of_commits()
+
+    part_of_day = {
+        "morning": part_of_day_percentage_of_commits.get(Part_Of_Day.MORNING, 0),
+        "afternoon": part_of_day_percentage_of_commits.get(Part_Of_Day.AFTERNOON, 0),
+        "evening": part_of_day_percentage_of_commits.get(Part_Of_Day.EVENING, 0),
+        "night": part_of_day_percentage_of_commits.get(Part_Of_Day.NIGHT, 0)
+    }
+
+    return Response(part_of_day)
