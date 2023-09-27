ec91af3824f8accc3272bac0ec452b6b957e9663
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-27 13:16:12 -0400

    refactor(ui): match current basic design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [stat, setStat] = useState("");
   const [username, setUsername] = useState("freddyhm");
   const [repoName, setRepoName] = useState("my-repo-stats-python");
   const [selectedOption, setSelectedOption] = useState("America/Montreal");
@@ -26,36 +27,54 @@ function App() {
       )
       .then((response) => {
         console.log(response);
+        setStat(response.data);
       })
-      .catch((error) => {});
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <div className="App">
-      <p>
-        Github.com/
-        <input
-          type="text"
-          placeholder="enter username"
-          value={username}
-          onChange={handleUsernameChange}
-        />
-        /
-        <input
-          type="text"
-          placeholder="enter repo name"
-          value={repoName}
-          onChange={handleRepoNameChange}
-        />
-      </p>
-      <p>
-        Timezone:
-        <select value={selectedOption} onChange={handleSelectChange}>
-          <option value="America/Montreal">America/Montreal</option>
-          <option value="America/Vancouver"> America/Vancouver</option>
-        </select>
-      </p>
-      <button onClick={getStats}>Get Stats</button>
+      <br />
+      <div>
+        <p>
+          Github.com/
+          <input
+            type="text"
+            placeholder="enter username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          /
+          <input
+            type="text"
+            placeholder="enter repo name"
+            value={repoName}
+            onChange={handleRepoNameChange}
+          />
+        </p>
+        <p>
+          Timezone:
+          <select value={selectedOption} onChange={handleSelectChange}>
+            <option value="America/Montreal">America/Montreal</option>
+            <option value="America/Vancouver"> America/Vancouver</option>
+          </select>
+        </p>
+        <button onClick={getStats}>Get Stats</button>
+      </div>
+      <br />
+      <div>
+        <h2>When are commits typically made during the day?</h2>
+        <br />
+        <h3>Morning: {stat ? stat.morning : 0}%</h3>
+        <br />
+        <h3>Afternoon: {stat ? stat.afternoon : 0}%</h3>
+        <br />
+        <h3>Evening: {stat ? stat.evening : 0}%</h3>
+        <br />
+        <h3>Night: {stat ? stat.night : 0}%</h3>
+      </div>
     </div>
   );
 }
