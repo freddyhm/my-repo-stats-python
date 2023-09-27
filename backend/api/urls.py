d9bd77e2cdc183717af79e63082e0a1905f04538
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-27 08:54:32 -0400

    fix: remove unused import

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import api_home, StatsAPIView
+from .views import api_home
 
 urlpatterns = [
     path('stats/username/<str:username>/repo/<str:repo>/', api_home) # localhost 
