1e019f1525cd0eb6cfbfdd160de429435fbc5309
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-28 12:36:08 -0400

    feat: add GET and POST functions

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,8 +9,11 @@ from api.services.commit_fetcher import Part_Of_Day
 from django.core.exceptions import ValidationError
 from django.core import validators
 
+from statreports.models import StatReport
+from statreports.serializers import StatReportSerializer
+
 @api_view(["GET"])
-def api_home(request, username, repo,  *args, **kwargs):
+def api_get(request, username, repo,  *args, **kwargs):
 
     timezone = request.query_params.get('timezone')
 
@@ -25,6 +28,37 @@ def api_home(request, username, repo,  *args, **kwargs):
     except ValidationError as e:
         return Response({"error": e.message}, status=status.HTTP_400_BAD_REQUEST)
     
+
+    
+    instance = StatReport.objects.get(username=username, reponame=repo)
+
+    if instance:
+        data = StatReportSerializer(instance).data
+    
+    
+
+    return Response(data)
+
+
+
+@api_view(["POST"])
+def api_post(request, *args, **kwargs):
+
+    timezone = request.data['timezone']
+    username = request.data['username']
+    repo = request.data['repo']
+
+    try:
+        validators.validate_slug(username)
+        validators.validate_slug(repo)
+    except ValidationError as e:
+        return Response({"error": "Username or repo name are not valid"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+        validate_timezone(timezone)
+    except ValidationError as e:
+        return Response({"error": e.message}, status=status.HTTP_400_BAD_REQUEST)
+
     try:
         part_of_day_percentage_of_commits = get_part_of_day_percentage_of_commits(username, repo, timezone)
 
@@ -41,8 +75,23 @@ def api_home(request, username, repo,  *args, **kwargs):
     except APIException:
         return Response("Could not fetch data from Github API", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    return Response(part_of_day)
+    data = {
+        "username" : username,
+        "reponame" : repo,
+        "timezone" : timezone,
+        "stat_content" : part_of_day
+    }
+
+    serializer = StatReportSerializer(data=data)
+    if serializer.is_valid():
+        instance = serializer.save()
+        print(instance)
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def validate_timezone(timezone):
     if timezone not in ["America/Montreal"]:
         raise ValidationError("Timezone is not valid")
+    
+
