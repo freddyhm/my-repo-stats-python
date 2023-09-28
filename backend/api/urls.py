09362e63fdf218fe43d01dee4027b90c1c91f88d
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2023-09-28 17:14:13 -0400

    feat: add happy path api tests

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import api_post, api_get
 
 urlpatterns = [
-    path('stats/username/<str:username>/repo/<str:repo>/', api_get),
-    path('stats/create', api_post)
+    path('stats/username/<str:username>/repo/<str:repo>/', api_get, name='api_get'),
+    path('stats/create', api_post, name='api_post')
 ]
